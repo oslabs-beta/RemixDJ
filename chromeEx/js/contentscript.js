af7f9bb47bd02653773a2607964a6bde3d41eedc
mjax <matt.d.jax@gmail.com>
2023-03-29 10:27:16 -0400

    trying to get content script loading remix manifest

--- chromeEx/js/contentscript.js
+++ chromeEx/js/contentscript.js
@@ -1,4 +1,15 @@
-alert('CS ALERT')
-const test = document.createElement('div')
-test.innerText='TEST INPUT'
-document.body.appendChild(test)
+// alert('CS ALERT')
+// // const test = document.createElement('div')
+// test.innerText='TEST INPUT'
+// document.body.appendChild(test)
+console.log('start')
+console.log(window)
+console.log(window["__remixContext"])
+document.addEventListener("DOMContentLoaded", (e) => {
+	console.log('event')
+	console.log(window)
+})
+setTimeout(() => {
+	console.log('timeout')
+	console.log(window["__remixContext"])
+}, 5000)
--- chromeEx/js/devtools.js
+++ chromeEx/js/devtools.js
@@ -1,6 +1,8 @@
 chrome.devtools.panels.create('REMIX', null, '/html/panel.html', null);
 chrome.devtools.inspectedWindow.getResources((reso)=> console.log(reso))
 console.log('after')
+const tabId = chrome.devtools.inspectedWindow.tabId;
+console.log(tabId)
 // const test = document.createElement('div')
 // test.innerText='TEST INPUT'
 // document.body.appendChild(test)
