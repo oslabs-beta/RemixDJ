bfeb25d4a83f39ee106d345c0ea8c15e55f81543
mjax <matt.d.jax@gmail.com>
2023-04-03 19:59:10 -0400

    trying to clean

--- main-page/app/styles/index.css
+++
@@ -1,9 +0,0 @@
-body {
-  color: hsl(0, 0%, 100%);
-  background-image: radial-gradient(
-    circle,
-    rgba(152, 11, 238, 1) 0%,
-    rgba(118, 15, 181, 1) 35%,
-    rgba(58, 13, 85, 1) 100%
-  );
-}
