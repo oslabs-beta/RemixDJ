d204d885e346f3c0d2d03ddcb33b8fb3659bf4f3
victoriadillman <victoria.dillman@gmail.com>
2023-04-04 17:06:19 -0400

    added buttons on dev tool

---
+++ chrome-extension/src/List.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <body>
+        <h1>This will be a List!</h1>
+      </body>
+    </div>
+  )
+}
+
+export default List;
