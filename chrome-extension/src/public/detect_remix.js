af56701c394d876e37ce6b455ac313c8226e24bc
mjax <matt.d.jax@gmail.com>
2023-04-04 11:42:20 -0400

    small change

--- chrome-extension/src/public/detect_remix.js
+++ chrome-extension/src/public/detect_remix.js
@@ -1,4 +1,4 @@
 if (window["__remixContext"]) {
-	console.log("feefifofum, i smell a remix in the dom")
+	console.log("feefifofum, i smell remix in the dom")
 	window.dispatchEvent(new CustomEvent("getRemixData", {detail: window.__remixManifest}));
 }
