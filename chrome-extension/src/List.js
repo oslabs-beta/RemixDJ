7833b2cd1f9c40a2a2cf2e0cb39ea6ca1dd2f0e0
Adam Liang <adam.liang@alumni.stonybrook.edu>
2023-04-05 10:57:45 -0400

    added list and style

--- chrome-extension/src/List.js
+++ chrome-extension/src/List.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import remixManifest from './treeRender/mockData';
 
 function List() {
+  const [data, setData] = useState(remixManifest);
+  // recursively render the data in a collapsible list
+  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details 
+  const renderData = (data) => {
+    if( typeof data === 'string' ) {
+      return <li>{data}</li>
+    }
+    if( typeof data === 'boolean' ) {
+      return <li>{ data ? 'true' : 'false' }</li>
+    }
+    if( typeof data === 'object' ) {
+      return Object.keys(data).map( (key) => {
+        return (
+          <li key={key}>
+            <details>
+              <summary>{key}</summary>
+              <ul>
+                {renderData(data[key])} 
+              </ul>
+            </details>
+          </li>
+        )
+      })
+    }
+  }
   return (
     <div>
-      <body>
-        <h1>This will be a List!</h1>
-      </body>
+      <h1> List </h1>
+      <span id="listData"> { renderData(data) } </span>
     </div>
   )
 }
