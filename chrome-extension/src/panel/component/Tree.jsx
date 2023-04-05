8d50e41a48fefcd76b482826d7e565864968c961
Adam Liang <adam.liang@alumni.stonybrook.edu>
2023-04-05 19:09:41 -0400

    fix classes and file extensions

--- chrome-extension/src/panel/component/Tree.jsx
+++ chrome-extension/src/panel/component/Tree.jsx
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
 import * as d3 from 'd3';
 import React, { useState } from 'react';
-import '../styles/Tree.css';
-// import remixManifest from './test-data';s
+import './App.css';
+// import remixManifest from './test-data';
 // import { render } from 'react-dom';
 import { useEffect, useRef } from 'react';
-import parseData from '../treeRender/parseDataFunc';
+import parseData from './treeRender/parseDataFunc';
 
 function Tree(props) {
   const [manifest, setManifest] = useState({});
@@ -31,22 +31,23 @@ function Tree(props) {
     //   .attr("cy", 70)
     //   .attr("r",  50)
 
-    const margin = { top: 10, right: 120, bottom: 10, left: 40 },
+    if (treeData.children.length !== 0){
+      const margin = { top: 10, right: 120, bottom: 10, left: 40 },
       width = 960 - margin.right - margin.left,
       height = 500 - margin.top - margin.bottom;
 
-    const treemap = d3.tree().size([height, width]);
-    let nodes = d3.hierarchy(treeData, d => d.children);
-    nodes = treemap(nodes);
+      const treemap = d3.tree().size([height, width]);
+      let nodes = d3.hierarchy(treeData, d => d.children);
+      nodes = treemap(nodes);
 
-    const svg = d3.select(ref.current).append("svg")
+      const svg = d3.select(ref.current).append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom),
       g = svg.append("g")
-        .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
-    const node = g.selectAll(".node")
+      const node = g.selectAll(".node")
       .data(nodes.descendants())
       .enter().append("g")
       .attr("class", d => "node" + (d.children ? " node--internal"
@@ -54,7 +55,7 @@ function Tree(props) {
       .attr("transform", d => "translate(" + d.y + "," +
         d.x + ")");
 
-    const link = g.selectAll(".link")
+      const link = g.selectAll(".link")
       .data(nodes.descendants().slice(1))
       .enter().append("path")
       .attr("class", "link")
@@ -68,30 +69,31 @@ function Tree(props) {
           + " " + d.parent.y + "," + d.parent.x;
       });
 
-    node.append("circle")
-      // .attr("r", d => 6)
-      .attr('r', 2.5)
-      .style("stroke", d => d.data.level)
-      .style("fill", d => d.data.level)
-      .attr('fill', (d) => (d._children ? '#555' : '#999'))
-      .attr('stroke-width', 10)
+      node.append("circle")
+        // .attr("r", d => 6)
+        .attr('r', 2.5)
+        .style("stroke", d => d.data.level)
+        .style("fill", d => d.data.level)
+        .attr('fill', (d) => (d._children ? '#555' : '#999'))
+        .attr('stroke-width', 10)
 
 
-    node.append("text")
-      .attr("dy", "0.31em")
-      .attr("x", (d) => (d._children ? -9 : 9))
-      .attr("text-anchor", (d) => (d._children ? "end" : "start"))
-      .text((d) => d.data.name)
-      .clone(true)
-      .lower()
-      // .attr("fill", "white")
-      .attr("stroke-linejoin", "round")
-      .attr("stroke-width", 3)
-    // .attr("stroke", "black")
-    // .attr("stroke", "rgb(26, 23, 24)")
+      node.append("text")
+        .attr("dy", "0.31em")
+        .attr("x", (d) => (d._children ? -9 : 9))
+        .attr("text-anchor", (d) => (d._children ? "end" : "start"))
+        .text((d) => d.data.name)
+        .clone(true)
+        .lower()
+        // .attr("fill", "white")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 3)
+      // .attr("stroke", "black")
+      // .attr("stroke", "rgb(26, 23, 24)")
 
-    const nodesAndText = d3.selectAll('.node', '.text');
-    nodesAndText.raise()
+      const nodesAndText = d3.selectAll('.node', '.text');
+      nodesAndText.raise()
+    }
   }, [manifest])
 
   return (
