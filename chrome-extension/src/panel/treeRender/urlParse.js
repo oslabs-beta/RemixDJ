79e71de26813dcb36d67b051ab7f8e3b0f6c8e11
Molly Greene <mollycgreene@gmail.com>
2023-04-07 10:36:13 -0400

    updated tim's layoutParse and urlParse w brackets

--- chrome-extension/src/panel/treeRender/urlParse.js
+++ chrome-extension/src/panel/treeRender/urlParse.js
@@ -1,65 +1,103 @@
-export default function routeUrls(remixManifest) {
-  let routesObj = remixManifest.routes;
-  // This makes an array of arrays of the keys of the remixManifest broken up into an array based off splitting it up
-  // Wherever a '/' is (AKA splitting it by the path). Will reformat this in a bit to work with v2 of remix and all the
-  // other route naming conventions
-  let myKeys = [];
-  for (const key in routesObj) {
-    // Root is omitted as root exists in all projects and will be hard coded into tree object
-    if (key !== 'root') {
-      myKeys.push(key.split(/[/.]/));
+// This function transforms the data pulled from the window.__remixManifest object into a nested object of parent and child nodes
+export default function layoutParse(remixManifest) {
+
+  // This function is used in the 'keySplitter' function below to re-join array elements with opening & closing brackets
+  function joiner(arrOfStrings, char, i = 0) {
+
+    if (i === arrOfStrings.length) return arrOfStrings
+
+    let temp = ''
+    let currentEl = arrOfStrings[i];
+    let nextEl = arrOfStrings[i + 1];
+
+    if (currentEl[currentEl.length - 1] === '[') {
+      temp += (currentEl + char + nextEl);
+      arrOfStrings.splice(i, 2, temp)
+      return joiner(arrOfStrings, char, i);
     }
+
+    else return joiner(arrOfStrings, char, i += 1);
   }
 
-  // The newObj which will contain all of our routes. Starts with a root which has children which will house the routes.
-  let newObj = {
-    name: 'root',
-    children: [],
-  };
+  // This function generates an array of arrays, with each subarray containing routes broken up with '.' or '/' (outside of []'s)
+  function keySplitter(remixManifest) {
 
-  // cache for colors:
-  const colors = {
-    0: 'rgb(224, 92, 115)',
-    1: 'rgb(225, 81, 86)', // red
-    2: 'orange',
-    3: 'rgb(246, 206, 75)', // yellow
-    4: 'rgb(135, 214, 117)', // green
-    5: 'rgb(121, 236, 232)', // blue
-    6: 'rgb(82, 144, 247)', // dark blue
-    7: 'rgb(199, 72, 204)', // violet
-  };
+    let myKeys = [];
+    for (const key in remixManifest) {
+      if (key != 'root') {
 
-  for (let i = 0; i < myKeys.length; i++) {
-    // Whenever we go to a new path we start by parsing through the roots children and go deeper when we need to
-    // This is just a string now so it can be manipulated easily later
-    let pathString = 'newObj.children';
-    for (let j = 0; j < myKeys[i].length; j++) {
-      // Now we evaluate the pathString as an actual JS command with eval
-      let path = pathString;
+        // For keys without escape brackets, split by '.' or '/' and push into myKeys array
+        if (!key.includes('[')) {
+          myKeys.push(key.split(/[/.]/))
+        }
 
-      // This checks if in the current path there is an object with a name key with the value of the current route we are on
-      // If this is not true we then push the object to the current childrens array
-      if (!path.find((e) => e.name === myKeys[i][j])) {
-        if (!(myKeys[i][j].slice(0) === '_')) {
-          path.push({
-            name: myKeys[i][j],
-            level: colors[j % 8],
-            children: [],
-          });
+        // For keys with escape brackets:
+        if (key.includes('[')) {
+          // Split by dot to account for potentially non-escaped dots
+          if (key.includes('.')) { }
+          let splitKeyDot = key.split('.')
+          // Re-join with joiner function
+          splitKeyDot = joiner(splitKeyDot, '.');
+          // After splitting and re-joining by dots, do the same for slashes
+          const holder = [];
+          splitKeyDot.forEach((el) => {
+            if (!el.includes('/')) {
+              holder.push(el);
+            } if (el.includes('/')) {
+              let splitKeySlash = el.split('/')
+              splitKeySlash = joiner(splitKeySlash, '/')
+              holder.push(...splitKeySlash)
+            }
+          })
+          myKeys.push(holder);
         }
       }
-      // We then check where in the current children array the object with the path we are on is
-      // We save that in numbah!
+    }
+    return myKeys;
+  }
+
+  const myKeys = keySplitter(remixManifest);
+
+  // The newObj will contain all of our routes. Starts with a root node which has a child array for additional routes
+  let newObj = {
+    'name': 'root',
+    'children': [],
+    'max': 0,
+    'widthSet': 1,
+  }
+
+  // cache for color assignment to each node. colors are matched to the remix.run website color scheme. 
+  const colors = {
+    0: 'rgb(225, 81, 86)', // red
+    1: 'rgb(246, 206, 75)', // yellow
+    2: 'rgb(135, 214, 117)', // green
+    3: 'rgb(121, 236, 232)', // turequoise
+    4: 'rgb(82, 144, 247)', // dark blue
+    5: 'rgb(199, 72, 204)', // magenta
+    6: 'rgb(230, 134, 149)', // lt pink
+    7: 'rgb(224, 92, 115)', // dk pink
+  }
+
+  for (let i = 0; i < myKeys.length; i++) {
+    let pathString = newObj.children;
+    for (let j = 0; j < myKeys[i].length; j++) {
+      let path = pathString;
+      newObj.widthSet = j;
+      if (!(path.find(e => e.name === myKeys[i][j]))) {
+        if (myKeys[i][j].slice(0) !== '_') {
+          path.push({ 'name': myKeys[i][j], 'children': [], level: (colors[j % 8]) });
+        }
+      }
       let numbah;
+      newObj.max = Math.max(newObj.max, path.length)
       for (let k = 0; k < path.length; k++) {
         if (path[k].name === myKeys[i][j]) {
           numbah = k;
-          // Then using numbah and some string manipulation we go deeper into the object by adding that numbah and the .children notation
-          // So that when we go back through the second loop again we are now one deeper into it which is what we want.
-          pathString = pathString[numbah][children];
         }
       }
+      pathString = pathString[numbah]['children'];
     }
   }
-  return newObj;
+  const treeData = newObj;
+  return treeData
 }
