778fde12e0b0a759a824e3546479fbe398d50b44
Adam Liang <adam.liang@alumni.stonybrook.edu>
2023-04-08 12:49:16 -0400

    added test.jsx

---
+++ chrome-extension/src/panel/treeRender/test.jsx
@@ -0,0 +1,9 @@
+import remixManifest from "./mockData";
+import parseData from "./parseDataFunc";
+
+console.log(parseData(remixManifest))
+
+let data = parseData(remixManifest);
+// render a list to visualize the data from remixManifest
+function renderList(data){
+}
