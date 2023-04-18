a4c08dc5eac6fe426d07aad000b67ad89351f8f0
Molly Greene <mollycgreene@gmail.com>
2023-04-18 15:38:27 -0400

    Co-authored-by: Victoria <victoriadillman@users.noreply.github.com>

--- main-page/tailwind.config.js
+++ main-page/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'black': '#1E1E1E',
+      'white': '#fffff',
+      'red': '#E15156',
+      'yellow': '#F6CE4B',
+      'green': '#87D675',
+      'turquoise': '#79ECE8',
+      'magenta': '#C748CC',
+    },
   },
   plugins: [],
 }
-
