0d15b574bbaf62059deec368240199b4a1b321c0
Timothy Muller <timothymuller@Timothys-MacBook-Pro-2.local>
2023-04-18 11:18:35 -0400

    typescript done! pleasegit st

---
+++ chrome-extension/src/public/detect_remix.js
@@ -0,0 +1,7 @@
+if (window.__remixManifest) {
+	console.log("💿 feefifofum 👹, i smell remix in the dom 💿")
+	window.dispatchEvent(new CustomEvent("getRemixData", {detail: window.__remixManifest}));
+} else {
+	console.log('❌💔 no 🚫🥺 remix😔❌');
+	window.dispatchEvent(new CustomEvent("getRemixData", {detail: {}}));
+}
