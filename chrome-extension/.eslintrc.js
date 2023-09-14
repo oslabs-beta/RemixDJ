47b11e826f1ad5a2721e541589bc316cad20115f
Adam Liang <me@adamliang.dev>
2023-09-13 22:40:07 -0400

    fix: style changes

--- chrome-extension/.eslintrc.js
+++ chrome-extension/.eslintrc.js
@@ -4,68 +4,59 @@ module.exports = {
     es6: true,
     node: true,
     webextensions: true,
-    amd: true
+    amd: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended'
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     // Add TypeScript parser options
-    project: 'chrome-extension/tsconfig.json',
+    project: "chrome-extension/tsconfig.json",
   },
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
+      files: ["*.ts", "*.tsx"],
       extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:react/recommended',
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
       ],
-      parser: '@typescript-eslint/parser',
+      parser: "@typescript-eslint/parser",
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+        ecmaVersion: "latest",
+        sourceType: "module",
         ecmaFeatures: {
-          jsx: true
+          jsx: true,
         },
       },
       rules: {
-        '@typescript-eslint/no-unused-vars': 'warn',
-        '@typescript-eslint/no-explicit-any': 'off',
-        'react/react-in-jsx-scope': 'off',
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
+        "react/react-in-jsx-scope": "off",
         //no display name
-        'react/display-name': 'off',
+        "react/display-name": "off",
       },
       settings: {
         react: {
-          version: 'detect',
+          version: "detect",
         },
-      }
+      },
     },
   ],
   rules: {
-    'no-console': 'off',
-    'no-alert': 'off',
-    'no-unused-vars': 'warn',
-    'prefer-const': 'warn',
-    semi: [
-      'error',
-      'always'
-    ],
-    quotes: [
-      'error',
-      'single'
-    ],
-    'react/display-name': 'off'
+    "no-console": "off",
+    "no-alert": "off",
+    "no-unused-vars": "warn",
+    "prefer-const": "warn",
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+    "react/display-name": "off",
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: "detect",
+    },
+  },
 };
--- chrome-extension/src/types.ts
+++ chrome-extension/src/types.ts
@@ -1,47 +1,47 @@
-import { NumberValue } from "d3";
+import { NumberValue } from 'd3'
 
-export type dataType = string | boolean | { [key: string]: any };
+export type dataType = string | boolean | { [key: string]: any }
 
 export type manifestObj = {
-    name: string;
-    max?: number;
-    widthSet?: number;
-    level: null | string;
-    children: null | manifestObj[];
-    x: number;
-    y: number;
-};
+  name: string
+  max?: number
+  widthSet?: number
+  level: null | string
+  children: null | manifestObj[]
+  x: number
+  y: number
+}
 
 export type nodeObj = {
-    parent: null | nodeObj;
-    children?: nodeObj[];
-    x: number;
-    y: number;
-    descendants(): { name: string }[];
-};
+  parent: null | nodeObj
+  children?: nodeObj[]
+  x: number
+  y: number
+  descendants(): { name: string }[]
+}
 
 export type listObj = {
-    name: string;
-    y: number;
-    x: number;
-    parent: null | listObj;
-};
+  name: string
+  y: number
+  x: number
+  parent: null | listObj
+}
 
 export type circleObj = {
-    name: string;
-    level: number;
-    data: null | circleObj;
-    _children: circleObj[];
+  name: string
+  level: number
+  data: null | circleObj
+  _children: circleObj[]
 }
 
 export type parseObj = {
-    name: string;
-    max?: number;
-    widthSet?: number;
-    level: null | string;
-    children: null | parseObj[];
+  name: string
+  max?: number
+  widthSet?: number
+  level: null | string
+  children: null | parseObj[]
 }
 
-export type windowObj = { remixManifest: { routes: manifestObj } };
+export type windowObj = { remixManifest: { routes: manifestObj } }
 
-export type windowObjUnderscore = { __remixManifest: { routes: manifestObj } };
+export type windowObjUnderscore = { __remixManifest: { routes: manifestObj } }
